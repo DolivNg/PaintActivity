1e5a1b5229468631eff93e8e004bcd71c82aef47
dev_m <doliv1092000@gmail.com>
2023-06-20 10:26:26 +0300

    Vasa 1. add to project MainMenuFragment 2. add ViewModel to MainMenuFragment 3. add MainMenuFragment to app_nav

---
+++ app/src/main/java/com/vassa/paintactivity/ui/fragments/menu/MainMenuFragment.kt
@@ -0,0 +1,33 @@
+package com.vassa.paintactivity.ui.fragments.menu
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vassa.paintactivity.R
+import com.vassa.paintactivity.ui.fragments.menu.viewmodel.MainMenuViewModel
+
+class MainMenuFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainMenuFragment()
+    }
+
+    private lateinit var viewModel: MainMenuViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainMenuViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/vassa/paintactivity/ui/fragments/menu/viewmodel/MainMenuViewModel.kt
@@ -0,0 +1,7 @@
+package com.vassa.paintactivity.ui.fragments.menu.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainMenuViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
