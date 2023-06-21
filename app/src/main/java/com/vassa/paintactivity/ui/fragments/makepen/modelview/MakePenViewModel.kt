6551bff706f85b4ee0109b27b38f6d93ffb46203
dev_m <doliv1092000@gmail.com>
2023-06-21 18:35:28 +0300

    Vasa 1. add fragment_make_pen.xml

---
+++ app/src/main/java/com/vassa/paintactivity/ui/fragments/makepen/modelview/MakePenViewModel.kt
@@ -0,0 +1,7 @@
+package com.vassa.paintactivity.ui.fragments.makepen.modelview
+
+import androidx.lifecycle.ViewModel
+
+class MakePenViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
