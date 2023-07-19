e386df312914d76e13369a836883c5f8fd6ef93b
dev_m <doliv1092000@gmail.com>
2023-07-19 16:57:10 +0300

    Vasa 1. create ChoosingAvatarDialog

---
+++ app/src/main/java/com/vassa/paintactivity/ui/fragments/option/dialogs/avatar/ChoosingAvatarDialog.kt
@@ -0,0 +1,83 @@
+package com.vassa.paintactivity.ui.fragments.option.dialogs.avatar
+
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.DialogFragment
+import com.vassa.paintactivity.databinding.DialogListTextBinding
+import com.vassa.paintactivity.databinding.LayoutSelectAvatarBinding
+import com.vassa.paintactivity.domain.entity.avatar.AvatarDomEntity
+import com.vassa.paintactivity.ui.constants.VectorAssets
+
+class ChoosingAvatarDialog(val chooser : ChooseAvatarCallBack) : DialogFragment() {
+
+    private var _binding: LayoutSelectAvatarBinding? = null
+    private val binding get() = _binding!!
+    private var columns = 4
+    private var row = VectorAssets.vectors.size / columns + 1
+    private var dataAvatar = ArrayList<AvatarDomEntity>()
+
+    //TODO TEST
+    init {
+        for (i in 0 until VectorAssets.vectors.size)
+            dataAvatar.add(AvatarDomEntity(i,i<5))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = LayoutSelectAvatarBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val displayMetrics = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val widthOfScreen = displayMetrics.widthPixels
+        val widthOfCell = widthOfScreen / (columns + 1)
+
+        binding.grLAvatar.apply {
+            columnCount = columns
+            rowCount = row
+        }
+
+        for (i in 0 until VectorAssets.vectors.size) {
+            val im = ImageView(context)
+            im.apply {
+                id = i
+                maxWidth = widthOfCell
+                maxHeight = widthOfCell
+                layoutParams = ViewGroup.LayoutParams(widthOfCell, widthOfCell)
+                if (i < dataAvatar.size)
+                    if (dataAvatar[i].open)
+                        setImageResource(VectorAssets.vectors[i])
+                    else
+                        setImageResource(VectorAssets.close)
+            }
+            im.setOnClickListener {
+                if (dataAvatar[im.id].open){
+                    chooser.avatarChoose(im.id)
+                    dismiss()
+                }
+            }
+            binding.grLAvatar.addView(im)
+            binding.bttnAvatarCancel.setOnClickListener {
+                dismiss()
+            }
+        }
+
+    }
+
+    interface ChooseAvatarCallBack {
+        fun avatarChoose(id : Int)
+
+    }
+}
